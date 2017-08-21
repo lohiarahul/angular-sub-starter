6489c96a86077752d9ddd0ee008a55c454793b58
Rahul Lohia <lohiarahul@hotmail.com>
2017-08-21 10:47:02 +1000

    Removed BrowserModule

--- src/app/subApp.module.ts
+++ src/app/subApp.module.ts
@@ -1,4 +1,3 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import {
@@ -60,7 +59,6 @@ type StoreType = {
    * Import Angular's modules.
    */
   imports: [
-    BrowserModule,
     FormsModule,
     HttpModule,
     RouterModule.forRoot(SUBROUTES)
