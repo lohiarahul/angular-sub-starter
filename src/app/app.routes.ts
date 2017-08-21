a7056441c9909a21ff4e200b037c802b027a58f1
Rahul Lohia <lohiarahul@hotmail.com>
2017-08-21 11:51:43 +1000

    loading app component instead

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,14 +2,14 @@ import { Routes } from '@angular/router';
 import { SubHomeComponent } from './subHome';
 import { AboutComponent } from './about';
 import { NoContentComponent } from './no-content';
-
+import { AppComponent } from './app.component';
 import { DataResolver } from './app.resolver';
 
 export const SUBROUTES: Routes = [
-  { path: '',      component: SubHomeComponent },
-  { path: 'home',  component: SubHomeComponent },
+  { path: '', component: AppComponent },
+  { path: 'home', component: SubHomeComponent },
   { path: 'about', component: AboutComponent },
-  { path: 'detail', loadChildren: './+detail#DetailModule'},
-  { path: 'barrel', loadChildren: './+barrel#BarrelModule'},
-  { path: '**',    component: NoContentComponent },
+  { path: 'detail', loadChildren: './+detail#DetailModule' },
+  { path: 'barrel', loadChildren: './+barrel#BarrelModule' },
+  { path: '**', component: NoContentComponent },
 ];
