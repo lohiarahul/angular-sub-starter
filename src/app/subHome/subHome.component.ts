e9432818b2190a19a6822413a65fac97592853cf
Rahul Lohia <lohiarahul@hotmail.com>
2017-08-21 13:57:19 +1000

    Load image

--- src/app/subHome/subHome.component.ts
+++ src/app/subHome/subHome.component.ts
@@ -27,7 +27,7 @@ import { XLargeDirective } from './x-large';
   /**
    * Every Angular template is first compiled by the browser before Angular runs it's compiler.
    */
-  template : `<img [src]="imagePath1" />
+  template : ` This is the sub component<img [src]="imagePath1" />
   <img [src]="imagePath2" />`
 })
 export class SubHomeComponent implements OnInit {
